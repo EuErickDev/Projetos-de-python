23dfd852095ac68067409a724bf7313a4739f371
Erick Felipe <euerickdev@gmail.com>
2026-01-22 10:55:03 -0300

    Create identificador_mes.py
    Este script utiliza a estrutura match-case (disponível a partir do Python 3.10) para converter um número inteiro no nome do mês correspondente. Inclui tratamento de erros para entradas não numéricas e validação para números fora do intervalo de 1 a 12.

---
+++ identificador_mes.py
@@ -0,0 +1,52 @@
+# Exibe as opções ao usuário
+print("DIGITE O NÚMERO CORRESPONDENTE AO MÊS:")
+print("""1 - JANEIRO
+2 - FEVEREIRO
+3 - MARÇO
+4 - ABRIL
+5 - MAIO
+6 - JUNHO
+7 - JULHO
+8 - AGOSTO
+9 - SETEMBRO
+10 - OUTUBRO
+11 - NOVEMBRO
+12 - DEZEMBRO""")
+
+# Lê e valida a entrada do usúario
+try:
+    numero = int(input("Sua escolha: "))
+
+    # Usa match-case para indentificar o mês
+    match numero:
+        case 1:
+            mes = "JANEIRO"
+        case 2:
+            mes = "FEVEREIRO"
+        case 3:
+            mes =  "MARÇO"
+        case 4:
+            mes = "ABRIL"
+        case 5:
+            mes = "MAIO"
+        case 6:
+            mes = "JUNHO"
+        case 7:
+            mes = "JULHO"
+        case 8:
+            mes = "AGOSTO"
+        case 9:
+            mes = "SETEMBRO"
+        case 10:
+            mes = "OUTUBRO"
+        case 11:
+            mes = "NOVEMBRO"
+        case 12:
+            mes = "DEZEMBRO"
+        case _:
+            mes = "MÊS INVÁLIDO"
+except ValueError:
+    mes = "ENTRADA INVÁLIDA, digite um número inteiro."
+
+# Exibe o resultado
+print("O mês selecionado é:",mes)
